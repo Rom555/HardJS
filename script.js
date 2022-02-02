c1aa913d6e65c538c5cd36ea92dcf47ae3138751
Rom555 <romanzubrilin@gmail.com>
2022-02-02 11:02:34 +0300

    complete lesson05

---
+++ script.js
@@ -0,0 +1,51 @@
+let arr = [
+  "21432143",
+  "13243245",
+  "1234353215",
+  "2342566",
+  "2356745",
+  "1234543",
+  "452662",
+];
+
+arr.forEach((element) => {
+  if (element[0] == 2 || element[0] == 4) {
+    console.log(element);
+  }
+});
+
+//2
+
+let prime = [];
+
+for (let i = 2; i <= 100; i++) {
+  let flag = true;
+
+  for (let element of prime) {
+    if (i % element === 0) flag = false;
+  }
+
+  if (flag) {
+    prime.push(i);
+    console.log(i);
+  }
+}
+
+// 2 оптимизировано под большие числа
+let primeBig = [];
+
+for (let i = 2; i <= 1000000; i++) {
+  let flag = true;
+  let sqrt = Math.ceil(Math.sqrt(i));
+
+  for (let element of primeBig) {
+    if (i % element === 0) flag = false;
+    if (element >= sqrt) {
+      break;
+    }
+  }
+
+  if (flag) {
+    primeBig.push(i);
+  }
+}
