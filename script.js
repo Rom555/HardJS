b0e88c417c66a7212073d578cfc3c2e106c6cdf1
Rom555 <romanzubrilin@gmail.com>
2022-02-17 15:11:53 +0300

    complete lesson 16

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+class First {
+  static hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+
+class Second extends First {
+  static hello() {
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+}
+
+Second.hello();
