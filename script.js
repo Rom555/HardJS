0e89ca743a78405b6d50d96b42b19fee5500b5ab
Rom555 <romanzubrilin@gmail.com>
2022-02-23 16:36:56 +0300

    complete lesson 19

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const input = document.getElementById('input');
+const p = document.getElementById('p');
+
+let timeoutId;
+
+input.addEventListener('keyup', function () {
+  if (timeoutId) clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    p.textContent = this.value;
+  }, 300);
+});
