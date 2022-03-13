239a10daf53163c4309c2f90161dee391cd0f1c6
Rom555 <romanzubrilin@gmail.com>
2022-03-13 18:28:51 +0300

    complete lesson28 cars

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const select = document.getElementById('select');
+const text = document.getElementById('text');
+
+const url = 'cars.json';
+
+const getData = () => {
+  return fetch(url).then((response) => response.json());
+};
+
+const renderCarData = (carBrand) => {
+  getData().then((data) => {
+    data.cars.forEach((car) => {
+      if (car.brand === carBrand) {
+        text.innerHTML = `
+          Тачка ${car.brand} ${car.model}<br>
+          Цена: ${car.price}$
+        `;
+      }
+    });
+  });
+};
+
+select.addEventListener('input', function () {
+  renderCarData(this[this.selectedIndex].value);
+});
+
+getData().then((data) => {
+  data.cars.forEach((car) => {
+    let option = document.createElement('option');
+    option.value = car.brand;
+    option.textContent = car.brand;
+
+    select.insertAdjacentElement('beforeend', option);
+  });
+});
